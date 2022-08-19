3514c0eda8b8633f6613c33abe40ec2a6c234bca
mk1encer <mksur1er@gmail.com>
2022-08-19 11:17:42 +0900

    login screen design

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Image, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import * as firebase from "firebase";
+import { signIn } from "./lib/auth";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signInSubmit = async () => {
+    try {
+      const { user } = await signIn({ email, passrowd });
+      Alert.alert(user);
+    } catch (e) {}
+    Alert.alert("로그인 실패");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello test!</Text>
+      <MaterialCommunityIcons name="truck-delivery" size={80} color="black" />
+      <TextInput
+        style={styles.inputBox}
+        placeholder="ID"
+        value={email}
+        autoCapitalize="none"
+        onChangeText={(email) => setEmail(email)}
+      />
+      <TextInput
+        style={styles.inputBox}
+        placeholder="Password"
+        value={password}
+        autoCapitalize="none"
+        onChangeText={(pw) => setPassword(pw)}
+      />
+      <Button title="Sign in" style={styles.button} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFF0F5",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputBox: {
+    height: 40,
+    width: 200,
+    backgroundColor: "white",
+    margin: 3,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+  },
+  button: {
+    borderWidth: 1,
+    fontSize: 24,
+    borderRadius: 8,
+    backgroundColor: "#4B0082",
   },
 });
