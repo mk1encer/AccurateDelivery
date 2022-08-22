38ed3b3c4db3cb35746e3143832450bd3a150467
mk1encer <mksur1er@gmail.com>
2022-08-22 13:37:15 +0900

    Camera module

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeScreen from '../screens/HomeScreen';
+import HomeScreen from "../screens/HomeScreen";
+import CameraScreen from "../screens/CameraScreen";
 
 const Stack = createStackNavigator();
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name='Home' component={HomeScreen} />
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Camera" component={CameraScreen} />
     </Stack.Navigator>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { IconButton } from '../components';
-import Firebase from '../config/firebase';
-import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
+import { IconButton } from "../components";
+import Firebase from "../config/firebase";
+import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
 
 const auth = Firebase.auth();
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   const { user } = useContext(AuthenticatedUserContext);
   const handleSignOut = async () => {
     try {
@@ -19,16 +19,22 @@ export default function HomeScreen() {
   };
   return (
     <View style={styles.container}>
-      <StatusBar style='dark-content' />
+      <StatusBar style="dark-content" />
       <View style={styles.row}>
-        <Text style={styles.title}>Welcome {user.email}!</Text>
+        <Text style={styles.title}>Welcome {user.email.split("@")[0]}!</Text>
         <IconButton
-          name='logout'
+          name="logout"
           size={24}
-          color='#fff'
+          color="#fff"
           onPress={handleSignOut}
         />
       </View>
+      <IconButton
+        name="camera"
+        size={40}
+        color="#fff"
+        onPress={() => navigation.navigate("Camera")}
+      />
       <Text style={styles.text}>Your UID is: {user.uid} </Text>
     </View>
   );
@@ -37,24 +43,24 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e93b81',
+    backgroundColor: "#8b00ff",
     paddingTop: 50,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
   },
   title: {
     fontSize: 24,
-    fontWeight: '600',
-    color: '#fff'
+    fontWeight: "600",
+    color: "#fff",
   },
   text: {
     fontSize: 16,
-    fontWeight: 'normal',
-    color: '#fff'
-  }
+    fontWeight: "normal",
+    color: "#fff",
+  },
 });
